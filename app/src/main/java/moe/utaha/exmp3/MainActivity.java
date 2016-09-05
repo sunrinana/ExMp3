7dcdb5ff5d6e4e0b23cb08ab374409b588bc1540
Jaeho Cho <real0131@gmail.com>
2016-09-05 23:31:11 +0900

    init

---
+++ app/src/main/java/moe/utaha/exmp3/MainActivity.java
@@ -0,0 +1,52 @@
+package moe.utaha.exmp3;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private ArrayAdapter<String> m_Adapter;
+    private ArrayList<String> list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        m_Adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1);
+        listView = (ListView) findViewById(R.id.list_item);
+
+        listView.setAdapter(m_Adapter);
+        Uri uri = Uri.parse("android.resource://com.cpt.sample/raw/");
+        Log.e("asdf --------------",uri.toString());
+        //for (String i : list )
+        //{
+        //    m_Adapter.add(i);
+        //}
+    }
+
+    public void getMp3list(){
+        Uri uri = Uri.parse("android.resource://com.cpt.sample/raw/");
+        list = new ArrayList<String>();
+        try
+        {
+            File file = new File("/sdcard/A");
+            File[] files = file.listFiles();
+            for(int i = 0;i < files.length;i++)
+            {
+                list.add(files[i].getName());
+            }
+        } catch( Exception e )
+        {
+            Toast.makeText(getApplicationContext(),"file error",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
